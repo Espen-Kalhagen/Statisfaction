43de21cdb7c014d96e80764bff960181566386dd
Espen Kalhagen <espesk15@uia.no>
2017-03-30 13:23:34 +0200

    Added RabbitMQ to StoreUnit

--- Statisfaction/ClientApp/widgets/components/smiley-widget/smiley-widget.component.ts
+++ Statisfaction/ClientApp/widgets/components/smiley-widget/smiley-widget.component.ts
@@ -1,20 +1,43 @@
 import { Component } from '@angular/core';
 
+declare var Stomp: any;
+declare var $: any;
+declare var send_data:any;
+declare var all: any;
+declare var start_rabbit: any;
+declare var send_wrapper: any;
+
+
 @Component({
     selector: 'widget-smiley',
     templateUrl: './smiley-widget.component.html',
     styleUrls: ["./smiley-widget.component.css"]
 })
 
+
 export class SmileyWidgetComponent 
 {
-    title:string = "Widget Title";
-    selection:string;
+    rabbitRunning:boolean = false;
+    title:string = "Widget name";
+    selection:string; 
+
+
+
 
     onSelect(option:string) : void
     {
         this.selection = option;
     }
+    send(smileyNr){
+        if (!this.rabbitRunning){
+            start_rabbit();
+            this.rabbitRunning=true;
+        }else{
+        send_wrapper(smileyNr);
+        }
+
+    }
+
 }
 
 
