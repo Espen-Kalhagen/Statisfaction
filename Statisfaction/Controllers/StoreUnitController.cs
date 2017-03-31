89dcfdbc80f78b4501c5f20e849ad63b655dc0e7
Espen Kalhagen <espesk15@uia.no>
2017-03-31 10:52:14 +0200

    Made rabbitMQ worker start a new thread, removed bower STAT-28

--- Statisfaction/Controllers/StoreUnitController.cs
+++ Statisfaction/Controllers/StoreUnitController.cs
@@ -4,7 +4,11 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+<<<<<<< HEAD
 using Data;
+=======
+using Statisfaction.Data;
+>>>>>>> Made rabbitMQ worker start a new thread, removed bower STAT-28
 
 namespace Controllers
 {
