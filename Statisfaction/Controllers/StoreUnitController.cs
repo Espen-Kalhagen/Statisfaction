e3af90915c3e0c77b50ed77ae39faef21a4a4979
Espen Kalhagen <espesk15@uia.no>
2017-03-31 10:52:14 +0200

    Made rabbitMQ worker start a new thread, removed bower STAT-28

--- Statisfaction/Controllers/StoreUnitController.cs
+++ Statisfaction/Controllers/StoreUnitController.cs
@@ -3,16 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Statisfaction.Data;
 
 namespace Statisfaction.Controllers
 {
     public class StoreUnitController : Controller
     {
+        //Must use this for db to not get disposed after request finishes
+        DbContextOptions<ApplicationDbContext> db;
 
-        ApplicationDbContext db;
-
-        public StoreUnitController(ApplicationDbContext db){
+        public StoreUnitController(DbContextOptions<ApplicationDbContext> db){
             this.db = db;
         }
         //Inputing data in the form will produce a rabbitMQ message
@@ -21,18 +22,19 @@ namespace Statisfaction.Controllers
 
             return View();
         }
-        //A sample post is created by calling this
-        public IActionResult Post()
-        {
-            new RabbitMQTasks.NewTask();
-            return View("Index");
-        }
         //Start a worker that consumes rabbitMQ messages (Writes them to the console), consumes everything in the queue and stays alive 
         //Having more wokrers spilits the work between the workers (round robin)
         //probably a good idea to find a more elegant way to start a new worker
         public IActionResult Read()
         {
-            new RabbitMQTasks.Worker(db);
+            var worker = new RabbitMQTasks.Worker(db);
+            System.Threading.Thread myThread;
+            myThread = new System.Threading.Thread(new
+                System.Threading.ThreadStart(worker.StartRead)); 
+
+            myThread.Start();
+             ViewBag.WorkerStatus = "Started worker";
+                
             return View("Index");
         }
 
