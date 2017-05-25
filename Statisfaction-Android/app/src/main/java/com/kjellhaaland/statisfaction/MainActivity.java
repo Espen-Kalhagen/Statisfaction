2cf5f3438c885d32092d5e64fde0b47bed81df39
Kjell Håland <kjellh14@uia.no>
2017-05-25 15:59:01 +0200

    Changed URL and AppIcon in Android app.

--- Statisfaction-Android/app/src/main/java/com/kjellhaaland/statisfaction/MainActivity.java
+++ Statisfaction-Android/app/src/main/java/com/kjellhaaland/statisfaction/MainActivity.java
@@ -1,16 +1,25 @@
 package com.kjellhaaland.statisfaction;
 
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.WindowManager;
+import android.webkit.WebChromeClient;
 import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "WebView";
+
     // URL pointing to the desired login-page
-    private final String URL_STARTUP = "http://www.uia.no";
+    private final String URL_STARTUP = "http://statisfaction3.azurewebsites.net/store-unit";
 
     // Reference to the container-webview
     private WebView container;
@@ -23,6 +32,7 @@ public class MainActivity extends AppCompatActivity {
 
         // Initialize the container web-view
         container = (WebView) findViewById(R.id.application_container);
+
 
         // Configure app
         this.configureContainer();
@@ -46,9 +56,50 @@ public class MainActivity extends AppCompatActivity {
      */
     private void configureContainer()
     {
+
+        WebSettings ws = this.container.getSettings();
+
+        ws.setJavaScriptEnabled(true);
+        ws.setAllowFileAccess(true);
+
+
+            try {
+                Log.d(TAG, "Enabling HTML5-Features");
+                Method m1 = WebSettings.class.getMethod("setDomStorageEnabled", new Class[]{Boolean.TYPE});
+                m1.invoke(ws, Boolean.TRUE);
+
+                Method m2 = WebSettings.class.getMethod("setDatabaseEnabled", new Class[]{Boolean.TYPE});
+                m2.invoke(ws, Boolean.TRUE);
+
+                Method m3 = WebSettings.class.getMethod("setDatabasePath", new Class[]{String.class});
+                m3.invoke(ws, "/data/data/" + getPackageName() + "/databases/");
+
+                Method m4 = WebSettings.class.getMethod("setAppCacheMaxSize", new Class[]{Long.TYPE});
+                m4.invoke(ws, 1024*1024*8);
+
+                Method m5 = WebSettings.class.getMethod("setAppCachePath", new Class[]{String.class});
+                m5.invoke(ws, "/data/data/" + getPackageName() + "/cache/");
+
+                Method m6 = WebSettings.class.getMethod("setAppCacheEnabled", new Class[]{Boolean.TYPE});
+                m6.invoke(ws, Boolean.TRUE);
+
+                Log.d(TAG, "Enabled HTML5-Features");
+            }
+            catch (NoSuchMethodException e) {
+                Log.e(TAG, "Reflection fail", e);
+            }
+            catch (InvocationTargetException e) {
+                Log.e(TAG, "Reflection fail", e);
+            }
+            catch (IllegalAccessException e) {
+                Log.e(TAG, "Reflection fail", e);
+            }
+
         // Loads the desired url into the webview
         container.loadUrl(URL_STARTUP);
 
+
+        container.setWebChromeClient(new WebChromeClient());
         // Listens to url-loading events (When the user clicks a URL in the webView)
         // Opens the clicked URL in the container
         container.setWebViewClient(new WebViewClient(){
@@ -60,8 +111,5 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-
-    //TODO: Load URL_STARTUP from server
-    //TODO:
 
 }
